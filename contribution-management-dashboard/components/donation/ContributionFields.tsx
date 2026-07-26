0c6e25dee0f40bc7681caf7604ab454d3dc8e6a7
Denish1981 <denishpatel1981@gmail.com>
2026-07-26 20:20:03 +0530

    Set maximum number of coupons to 5

--- contribution-management-dashboard/components/donation/ContributionFields.tsx
+++ contribution-management-dashboard/components/donation/ContributionFields.tsx
@@ -77,7 +77,8 @@ export const ContributionFields: React.FC<ContributionFieldsProps> = ({
                             onChange={e => setNumberOfCoupons(e.target.value)} 
                             className="mt-1 block w-full px-3 py-2 border border-slate-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500" 
                             required 
-                            min="0" 
+                            min="0"
+                            max="5"
                         />
                     </div>
                 )}
